e9130e23f9f5191087e732e347147cf8e47f30fe
Capone23g <71262792+Capone23g@users.noreply.github.com>
2023-05-02 12:33:25 +0300

    -Added a TextField composable to the UI to let the user convert their own temperatures. -Applied default material theme. -Centered only the button by first nesting it inside a Row composable

--- app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
@@ -5,57 +5,57 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.hfad.temperatureconverter.ui.theme.TemperatureConverterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            mainActivityComponent()
+            MaterialTheme {
+                Surface {
+                    MainActivityContent()
+                }
 
-
-
+            }
 
         }
     }
 }
+
 @Composable
-fun Header(image: Int,description: String){
+fun Header(image: Int, description: String) {
     Image(
         painter = painterResource(image), // Define the drawable to use
-        contentDescription = description ,// description of image For accessibility
-    modifier = Modifier
-        .height(180.dp)
-        .fillMaxWidth(),
+        contentDescription = description,// description of image For accessibility
+        modifier = Modifier
+            .height(180.dp)
+            .fillMaxWidth(),
         contentScale = ContentScale.Crop
-
 
 
     )
 
 
+}
+
+@Composable
+fun TemperatureText(celsius: Int) {
+    val fahrenheit = (celsius.toDouble() * 9 / 5) + 32
+    Text("$celsius Celsius is $fahrenheit Fahrenheit")
 
 
 }
-@Composable
-fun TemperatureConverter(celsius:Int){
-    val farenheit = (celsius.toDouble() * 9/5) + 32
-    Text("$celsius Celsius is $farenheit Farenheit")
 
-
-}
 @Composable
-fun convertButton(clicked: () -> Unit){
+fun ConvertButton(clicked: () -> Unit) {
     Button(onClick = clicked) {
         Text("Convert")
 
@@ -65,36 +65,60 @@ fun convertButton(clicked: () -> Unit){
 }
 
 
-
-
-
 @Composable
-fun mainActivityComponent(){
-    Column {
+fun MainActivityContent() {
+    val celsius = remember { mutableStateOf(0) }
+    val newCelsius = remember{ mutableStateOf("")}
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()) {
         Header(image = R.drawable.sunrise, description = "sunrise image")
-        TemperatureConverter(celsius = 0)
+        EnterTemperature(temperature = newCelsius.value) { newCelsius.value = it }
+
+        Row(modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center) {
+            ConvertButton {
+                newCelsius.value.toIntOrNull()?.let {
+                    //If newCelsius value is an int, assign it to celsius value
+                    celsius.value = it
+                }
+            }
+
+        }
+
+        TemperatureText(celsius.value)
+
 
     }
 
 
+}
+@Composable
+fun EnterTemperature(temperature: String,changed: (String) -> Unit ){
+    TextField(
+        value = temperature,
+        onValueChange = changed,
+        label = {Text("Enter a temperature in celsius ")},
+        modifier = Modifier.fillMaxWidth()
 
-
-
-
+    )
 
 
 }
+
+
 
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewMainActivity() {
-    mainActivityComponent()
+    MainActivityContent()
 
 
 }
 
-/*@Composable
+/*
+@Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
